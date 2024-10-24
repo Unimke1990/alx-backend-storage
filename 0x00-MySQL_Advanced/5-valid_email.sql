4b0c2939ca8a070883cc7c4a45a11e834282e814
Unimke1990 <agimunimke1990@gmail.com>
2024-10-24 16:40:21 +0200

    checks for validity of email

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,10 +1,11 @@
 -- trigger that reset email status
+DROP TRIGGER IF EXISTS reset_email;
 DELIMITER //
 CREATE TRIGGER reset_email
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-    IF NEW.email != OLD.email THEN
+    IF OLD.email != NEW.email THEN
 	SET valid_email = FALSE
 	WHERE id = NEW.id;
     END IF;
