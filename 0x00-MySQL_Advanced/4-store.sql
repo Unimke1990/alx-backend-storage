f9c99f322d19eb452d18e901de1a219e6526c8b7
Unimke1990 <agimunimke1990@gmail.com>
2024-10-24 14:35:50 +0200

    trigger that updates items quantity

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,13 @@
+-- a trigger that decreases the quantity of an item after adding a new order.
+DELIMITER //
+
+CREATE TRIGGER decrease_item_qauntity_after_order
+AFTER INSERT ON orders
+FOR EACH ROW ON
+BEGIN
+UPDATE items
+SET quantity = quantity - NEW.quantity_ordered
+WHERE id = items_id;
+END//
+
+DELIMITER ;
