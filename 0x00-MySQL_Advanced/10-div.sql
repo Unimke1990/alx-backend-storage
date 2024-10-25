26e431c97a79d23b7fb4caf5ccc3d0a96e425d24
Unimke1990 <agimunimke1990@gmail.com>
2024-10-25 11:19:37 +0200

    created a function

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,12 @@
+-- creates a function
+DROP FUNCTION IF EXISTS SafeDiv;
+DELIMITER //
+CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS DECIMAL(10, 2);
+BEGIN
+    IF (b == 0) THEN
+        RETURN 0;
+    ELSE
+        RETURN a / b;
+    END IF;
+END //
+DELIMITER //
