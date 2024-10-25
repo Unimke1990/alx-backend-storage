c09e060e4ffaff7233bbee56a23aa3c45838990b
Unimke1990 <agimunimke1990@gmail.com>
2024-10-25 09:39:22 +0200

    email validation

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -6,8 +6,7 @@ BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
     IF OLD.email != NEW.email THEN
-	SET NEW.valid_email = FALSE
-	WHERE id = NEW.id;
+	SET NEW.valid_email = FALSE;
     END IF;
 END //
 DELIMITER ;
