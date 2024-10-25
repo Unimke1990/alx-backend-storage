92aa2d179cab2a57b00bdbb5c3fd419c742f724a
Unimke1990 <agimunimke1990@gmail.com>
2024-10-25 11:46:32 +0200

    created a function

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,12 +1,15 @@
--- creates a function
+-- creates a function SafeDiv that divides (and returns) the first
+-- by the second number or returns 0 if the second number is equal to 0.
 DROP FUNCTION IF EXISTS SafeDiv;
-DELIMITER //
-CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT DETERMINISTIC
+DELIMITER $$
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
 BEGIN
-    IF (b == 0) THEN
-        RETURN 0;
-    ELSE
-        RETURN a / b;
+    DECLARE result FLOAT DEFAULT 0;
+
+    IF b != 0 THEN
+        SET result = a / b;
     END IF;
-END //
+    RETURN result;
+END $$
 DELIMITER ;
