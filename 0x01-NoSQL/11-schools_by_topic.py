f7f57a55d04dd8aa9f33f2343ba9a25061d4b6ed
Unimke1990 <agimunimke1990@gmail.com>
2024-10-29 15:50:51 +0200

    function that returns a list of topics

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -4,5 +4,5 @@
 
 def schools_by_topic(mongo_collection, topic):
     """returns a list"""
-    school_list = mongo_collection.find({topics: "topic"})
+    school_list = mongo_collection.find({"topics": topic})
     return school_list
